176f21a598832632160288fe0804b9166c93af1c
niki <nikolayp1707@gmail.com>
2021-11-01 13:38:00 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import "./App.css";
+import {useState , useMemo} from "react";
 
 function App() {
+  let [text, setText] = useState("");
+
+  let tru = false;
+
+  const reg = new RegExp("^[0-9]+$")
+
+  const check =
+  useMemo(() =>{
+    if(reg.test(text) == false){
+      tru=false;
+    } else {
+      tru=true;
+    }
+  },[text, setText])
+
   return (
     <div className="App">
      <div className="control has-icons-right">
@@ -8,11 +24,16 @@ function App() {
           className="input is-large"
           type="text"
           placeholder="Enter number..."
+          value={text}
+          onChange={
+            (e) =>{
+            setText(e.target.value)
+          }}
 
         />
         <span className="icon is-small is-right">
-          <i className="fas fa-times" />
-        </span> 
+          <i className={tru ? "fas fa-check" : "fas fa-times"} />
+        </span>
       </div>
     </div>
   );
